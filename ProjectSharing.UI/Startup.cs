398de351e717f656a4ff511d37145f01db607af8
Adem Dölaslan <ademdolaslan@gmail.com>
2019-12-03 18:03:14 +0300

    trying revert

--- ProjectSharing.UI/Startup.cs
+++ ProjectSharing.UI/Startup.cs
@@ -50,6 +50,10 @@ namespace ProjectSharing.UI
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
             });
         }
     }
