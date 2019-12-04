48bbf68dffbc31441838b0ddd4c34130a3646af8
Adem Dölaslan <ademdolaslan@gmail.com>
2019-12-04 14:40:03 +0300

    Ftp dosya kaydetme, sayfayı kaydetme, varolan sayfaları güncelleme işlemleri yapılacak

---
+++ ProjectSharing.UI/Controllers/ProjectController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjectSharing.UI.Models;
+
+namespace ProjectSharing.UI.Controllers
+{
+    [Authorize(Roles ="Admin,User,Editor")]
+    public class ProjectController : Controller
+    {
+        public IActionResult Share()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SavePage(HtmlTextViewModel model)
+        {
+
+            return Json(model.HtmlText);
+
+        }
+    }
+}
---
+++ ProjectSharing.UI/Models/HtmlTextViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSharing.UI.Models
+{
+    public class HtmlTextViewModel
+    {
+        public string HtmlText { get; set; }
+        public IFormFile PageFiles { get; set; }
+    }
+}
